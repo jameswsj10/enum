ddb57437c22bdb82de849b6d9ef5e1f686b37ba3
James Jung <jameswsj@berkeley.edu>
2019-10-25 23:32:07 -0700

    adding in backend python files

---
+++ data_extraction/webscrape.py
@@ -0,0 +1,5 @@
+# python file for scraping websites
+# Input: URLs
+# Output: JSON files in format of d3 API
+
+## use google cloud API to extract data and create relations among subtopics of exctracted data
