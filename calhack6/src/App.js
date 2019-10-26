e0f2801a4974c301eeeb0c19ab1fe94f65687b0a
Deepali Jain <deepali.jain.sd@gmail.com>
2019-10-26 00:46:44 -0700

    work in progress - currently need to change bar chart to network graph

--- calhack6/src/App.js
+++ calhack6/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NetworkGraph from './NetworkGraph';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ENUM
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <NetworkGraph data={[5,10,1,3]} size={[500,500]} />
+        </div>
       </header>
     </div>
   );
