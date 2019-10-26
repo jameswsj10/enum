a30abfd77f08030c7d6fffbaa8da54c6c7a625b4
James Jung <jameswsj@berkeley.edu>
2019-10-26 03:52:32 -0700

    working on the extracting pdf files from website

--- data_extraction/webscrape.py
+++ data_extraction/webscrape.py
@@ -1,3 +1,9 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import re
+import os
+import urllib
+
 # python file for scraping websites
 # Input: URLs
 # Output: JSON files in format of d3 API
@@ -15,7 +21,15 @@
 #
 # Possible problems:
 # 1) Extracted keywords aren't really relevant to the actual material
-# -> Autism example in data 100, might catch autism as a keyword 
+# -> Autism example in data 100, might catch autism as a keyword
 # -> We need to figure out a way to remove these words from our output file
-#
-#
+
+
+def webscrape(url):
+    # connect to website and get list of all pdfs
+    response = request.urlopen(url).read()
+    soup= BeautifulSoup(response, "html.parser")
+    links = soup.find_all('a', href=re.compile(r'(.pdf)'))
+
+    # convert pdf files to text files while filtering the ones with  ‘discussion’ and ‘homework’ and ‘book’ in the name
+    a = "d
