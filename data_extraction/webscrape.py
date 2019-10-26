60924272bd344bd4bfc3cc115d5713881e0b6132
James Jung <jameswsj@berkeley.edu>
2019-10-25 23:47:29 -0700

    thoughts about web scraping procedure

--- data_extraction/webscrape.py
+++ data_extraction/webscrape.py
@@ -2,4 +2,20 @@
 # Input: URLs
 # Output: JSON files in format of d3 API
 
-## use google cloud API to extract data and create relations among subtopics of exctracted data
+# ## use google cloud API to extract data and create relations among subtopics of exctracted data
+#
+# Within each website, possible formats for where content is stored is
+# 1) PDFs (lecture notes, textbooks)
+# 2) Google Slides "....docs.google.com"
+# 3) HTML / online textbooks ".....html" files
+#
+# when extracting these three types of data, we look for certain keywords such as
+# "lecture", "schedule", "notes", etc.
+#
+#
+# Possible problems:
+# 1) Extracted keywords aren't really relevant to the actual material
+# -> Autism example in data 100, might catch autism as a keyword 
+# -> We need to figure out a way to remove these words from our output file
+#
+#
