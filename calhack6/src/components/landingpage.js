d1602626be1324fa6ee3e5ada4c56773fbd9d444
hirenpatel <nex.hirenpatel@gmail.com>
2019-10-26 22:44:41 -0700

    _

--- calhack6/src/components/landingpage.js
+++ calhack6/src/components/landingpage.js
@@ -4,7 +4,8 @@ import Graph from '../Graph';
 
 
 class Landing extends Component {
-  render() {
+
+	render() {
 	return(
 		<div style={{width: '100%', height: '100%', margin: 'auto'}}>
 			<Graph/>
