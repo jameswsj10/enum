aa814d36ad6b8768c860da9c041551d2673596db
hirenpatel <nex.hirenpatel@gmail.com>
2019-10-28 08:38:32 -0700

    _

--- calhack6/src/results.js
+++ calhack6/src/results.js
@@ -2,30 +2,93 @@ var data = {
     "nodes": [
     {
       "id": 1,
-      "title": "Node A",
-      "x": 258.3976135253906,
-      "y": 331.9783248901367,
+      "title": "Introduction & Logic",
+      "x": 300,
+      "y": 200,
       "type": "empty"
     },
     {
       "id": 2,
-      "title": "Node B",
-      "x": 593.9393920898438,
-      "y": 260.6060791015625,
+      "title": "Proofs",
+      "x": 200,
+      "y": 400,
       "type": "empty"
     },
     {
       "id": 3,
-      "title": "Node C",
-      "x": 237.5757598876953,
-      "y": 61.81818389892578,
+      "title": "Induction",
+      "x": 400,
+      "y": 600,
       "type": "empty"
     },
     {
       "id": 4,
-      "title": "Node C",
-      "x": 600.5757598876953,
-      "y": 600.81818389892578,
+      "title": "Stable Marriage",
+      "x": 200,
+      "y": 700,
+      "type": "empty"
+    },
+    {
+      "id": 5,
+      "title": "Graphs I",
+      "x": 0,
+      "y": 800,
+      "type": "empty"
+    },
+    {
+      "id": 6,
+      "title": "Graphs II",
+      "x": 200,
+      "y": 1000,
+      "type": "empty"
+    },
+    {
+      "id": 7,
+      "title": "Modular Arithmetic",
+      "x": 400,
+      "y": 800,
+      "type": "empty"
+    },
+    {
+      "id": 8,
+      "title": "Public Key Cryptography: RSA",
+      "x": 600,
+      "y": 800,
+      "type": "empty"
+    },
+    {
+      "id": 9,
+      "title": "Polynomials",
+      "x": 500,
+      "y": 1000,
+      "type": "empty"
+    },
+    {
+      "id": 10,
+      "title": "Error-Correcting Codes",
+      "x": 700,
+      "y": 1100,
+      "type": "empty"
+    },
+    {
+      "id": 11,
+      "title": "Countability",
+      "x": 1000,
+      "y": 1000,
+      "type": "empty"
+    },
+    {
+      "id": 12,
+      "title": "Counting I",
+      "x": 900,
+      "y": 1200,
+      "type": "empty"
+    },
+    {
+      "id": 13,
+      "title": "Counting II",
+      "x": 1100,
+      "y": 1200,
       "type": "empty"
     }
   ],
@@ -37,7 +100,72 @@ var data = {
     },
     {
       "source": 2,
+      "target": 3,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 1,
+      "target": 3,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 1,
+      "target": 3,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 3,
       "target": 4,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 2,
+      "target": 5,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 5,
+      "target": 6,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 3,
+      "target": 7,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 7,
+      "target": 8,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 3,
+      "target": 8,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 8,
+      "target": 9,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 9,
+      "target": 10,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 10,
+      "target": 11,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 11,
+      "target": 12,
+      "type": "emptyEdge"
+    },
+    {
+      "source": 11,
+      "target": 13,
       "type": "emptyEdge"
     }
   ]
